37282191cd32cd9f02c6de56840b15775028e814
Erin Dustin <erincdustin@gmail.com>
2019-08-01 14:17:49 -0500

    added styling

--- src/components/Enter/Enter.js
+++ src/components/Enter/Enter.js
@@ -4,8 +4,8 @@ import './Enter.css';
 class Header extends React.Component{
   render() {
     return (
-    <div className="flex-container">
-      <div className="flex">
+    <div className="outer">
+      <div className="inner">
           <h1>Hi, my name is Erin</h1>
           <h2>I followed my dream and pursued software engineering full-time. Now I build full stack web applications.</h2>
       </div>
