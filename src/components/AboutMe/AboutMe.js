73c84b21d6383ffe090e2d132b93d5f235ac74a6
Erin Dustin <erincdustin@gmail.com>
2019-08-01 09:33:51 -0500

    responsive Enter and AboutMe components

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import me from '../../images/portfolio-picture.jpg'
+
+import './AboutMe.css';
 
 function AboutMe(props){
   return (
     <div>
+      <h1 className="about-me-header">About Me</h1>
       <section role="region" id="about-me">
-        <h2 class="about-me-header">About Me</h2>
-        <div class = "container about-me">
-            {/* <img src="images/portfolio-picture.jpg" class="headshot" alt="image of Erin" /> */}
-            <p class="text">As a former marketer and salesperson, I am passionate about forging relationships and making visions come to life.  My enthusiasm for programming began at a young age when I first created content to mimic my favorite websites.  That fascination has stayed with me throughout the years, and has inspired me to pursue a career change that offers high levels of creativity and problem solving.  Above all, I love learning new things!  When I’m not behind a computer, you can find me outdoors at a bluegrass concert, rock climbing, or exploring the nearest hiking trail.</p>
+        <div className="headshot">
+        <img src={me} className="headshot-img" alt="Erin" />
+        </div>
+        <div className="about-me">
+          <p className="about-me-text">As a former marketer and salesperson, I am passionate about forging relationships and making visions come to life.  My enthusiasm for programming began at a young age when I first created content to mimic my favorite websites.  That fascination has stayed with me throughout the years, and has inspired me to pursue a career change that offers high levels of creativity and problem solving.  Above all, I love learning new things!  When I’m not behind a computer, you can find me outdoors at a bluegrass concert, rock climbing, or exploring the nearest hiking trail.</p>
         </div>
       </section>
     </div>
--- src/components/Enter/Enter.js
+++ src/components/Enter/Enter.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import me from '../../images/zion.jpg';
-import face from '../../images/portfolio-picture.jpg'
 import './Enter.css';
 
 class Header extends React.Component{
@@ -9,10 +6,8 @@ class Header extends React.Component{
     return (
     <div className="flex-container">
       <div className="flex">
-        {/* <div className="img"><img src={face} alt="me"/></div> */}
-        {/* <img src={me} alt="zion" className="picture"></img> */}  
           <h1>Hi, my name is Erin</h1>
-          <h2>I'm a full stack engineer</h2>
+          <h2>I followed my dream and pursued software engineering full-time. Now I build full stack web applications.</h2>
       </div>
     </div>
   )
