de790409bc9fcac5bf762f6c4f545888fe670880
Erin Dustin <erincdustin@gmail.com>
2019-08-02 01:13:16 -0400

    styling changes to all pages

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,8 +5,9 @@ import './Contact.css'
 
 function Contact(props){
   return (
-    <section role="region" id="contact">
-      <div class = "container">
+    <section id="contact">
+      <h1 class="connect-text">Let's Get Connected!</h1>
+      <div class = "container connect">
         <a href="https://www.linkedin.com/in/erin-dustin/" target="_blank"><img src="https://dumielauxepices.net/sites/default/files/linkedin-clipart-transparent-666582-6184783.png" class="linkedin icon" alt="link to LinkedIn" /><p>/erindustin</p></a>
         <a href="https://github.com/erincdustin" target="_blank"><img src="https://png.pngtree.com/svg/20170920/41a529ce9e.png" class="github icon" alt="link to Github" /><p>/erincdustin</p></a>
         <a href="mailto:erincdustin@gmail.com" target="_top" class="contact-email" ><img src={mail} class="email icon" alt="link to email" /><p>Email</p></a>         
--- src/components/Enter/Enter.js
+++ src/components/Enter/Enter.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import hand from '../../images/emoji.PNG'
 import './Enter.css';
 
 class Header extends React.Component{
   render() {
     return (
-    <div className="outer">
       <div className="inner">
-          <h1>Hi, my name is Erin</h1>
+        <div className="textbox">
+          <div className="banner">
+            <h1 className="header-homepage">
+              <div className="emoji">
+                <img className="emoji" src={hand} alt="waving hand emoji"></img>
+              </div> Hi, my name is Erin</h1>
+          </div>
           <h2>I followed my dream and pursued software engineering full-time. Now I build full stack web applications.</h2>
+        </div>
       </div>
-    </div>
   )
 }
 }
