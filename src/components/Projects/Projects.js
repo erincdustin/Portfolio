7a6068e95cf2541c09cbf3bee25101f50b473a51
Erin Dustin <erincdustin@gmail.com>
2019-09-12 17:25:30 -0400

    added demo usernames and passwords to all apps

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,6 +26,10 @@ function Projects(props){
             recommendations algorithm and posts a custom playlist to your account that fits the weather "mood". 
             Users can choose from a list of genres or their top Spotify artists to seed the playlist, and can
             alter mood settings like tempo and happiness if they want to change the song selections.</p>
+          <div className="demo">
+            <p><span className="demo-credentials">Demo Email:</span> wind.chime.app@gmail.com </p>
+            <p><span className="demo-credentials">Demo Password:</span> windchime123</p>
+          </div>
           <a href="https://erins-windchime-app.erincdustin.now.sh/" className="link-text" rel="noopener noreferrer" target="_blank"><button type="button">Demo</button></a>
           <a href="https://github.com/erincdustin/windchime-client" className="link-text" rel="noopener noreferrer" target="_blank"><button type="button">Client</button></a>
           <a href="https://github.com/erincdustin/windchime-server" className="link-text" rel="noopener noreferrer" target="_blank"><button type="button">Server</button></a>
@@ -33,21 +37,25 @@ function Projects(props){
         </section>
         <section className="project 2">
         <div className="thumbnail">
-          <a href="https://jobreel.now.sh/dashboard" rel="noopener noreferrer" target="_blank"><img src={jobreel} alt="JobReel app" className="portfolio" /></a>
+          <a href="https://jobreelapp.erincdustin.now.sh" rel="noopener noreferrer" target="_blank"><img src={jobreel} alt="JobReel app" className="portfolio" /></a>
           </div>
           <div className="info">
           <h1>JobReel</h1>
           <img src={react} alt="react symbol" className="stack"/>
           <img src={node} alt="node symbol" className="stack" /> 
-          <img src={postgres} alt="postgreSQL symbol" className="stack" />
+          <img src={postgres} alt="postgreSQL symbol" className="stack" /> 
           <p className="desc">JobReel simplifies the job search process for bootcamp graduates. 
           Users can search for job postings, networking events, and professional contacts within 
           the app and directly add these to their saved assets.  JobReel's database stores 
           jobs, contacts, companies of interest, events, and helpful resources and tracks 
           progress for the user.  Utilizes GitHub Jobs, Autherntic Jobs, Eventbrite, and Hunter.io 
           APIs.</p>
-          <a href="https://jobreel.now.sh/dashboard" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Demo</button></a>
-          <a href="https://github.com/thinkful-ei-dragonfly/JobReel-client" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Client</button></a>
+          <div className="demo">
+              <p><span className="demo-credentials">Demo Username:</span> Demo </p>
+              <p><span className="demo-credentials">Demo Password:</span> Asdf123!</p>
+          </div>
+          <a href="https://jobreelapp.erincdustin.now.sh" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Demo</button></a>
+          <a href="https://github.com/erincdustin/JobReel-client" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Client</button></a>
           <a href="https://github.com/thinkful-ei-dragonfly/JobReel-server" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Server</button></a>
           </div>
         </section>
@@ -62,6 +70,10 @@ function Projects(props){
             <img src={postgres} alt="postgreSQL symbol" className="stack" />
             <p className="desc">Apprendere is a flashcard app that uses spaced repetition to help users learn Italian.  After prompting for an answer to a vocabulary word, 
             the server will implement an algorithm to move the card back in the list, depending on whether the user answers correctly or incorrectly. </p>
+            <div className="demo">
+              <p><span className="demo-credentials">Demo Username:</span> demo </p>
+              <p><span className="demo-credentials">Demo Password:</span> Passw0rd!</p>
+            </div>
             <a href="https://apprendere.lucasvocos.now.sh/" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Demo</button></a>
             <a href="https://github.com/thinkful-ei-dragonfly/lucas-erin-spaced-repetition-client" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Client</button></a>
             <a href="https://github.com/thinkful-ei-dragonfly/lucas-erin-spaced-repetition-api" className="link-text" rel="noopener noreferrer" target="_blank"><button type="click">Server</button></a>
